00fcb26ae56b774dd741b8d2d5fc983d5b523376
caifu23 <1224758467@qq.com>
2019-12-07 16:59:12 +0800

    封装基地址http.js
    便于请求使用地址

---
+++ admin/libs/http.js
@@ -0,0 +1,31 @@
+;(function(w) {
+    let baseUrl = 'http://localhost:8080/api/v1';
+    let pathObj = {
+        baseUrl: baseUrl,
+        'user-login': baseUrl + '/admin/user/login',
+        'user-info': baseUrl + '/admin/user/info',
+        'user-detail': baseUrl + '/admin/user/detail',
+        'user-edit': baseUrl + '/admin/user/edit',
+        'category-list': baseUrl + '/admin/category/list',
+        'category-add': baseUrl + '/admin/category/add',
+        'category-search': baseUrl + '/admin/category/search',
+        'category-edit': baseUrl + '/admin/category/edit',
+        'category-delete': baseUrl + '/admin/category/delete',
+        'article-query': baseUrl + '/admin/article/query',
+        'article-publish': baseUrl + '/admin/article/publish',
+        'article-search': baseUrl + '/admin/article/search',
+        'article-edit': baseUrl + '/admin/article/edit',
+        'article-delete': baseUrl + '/admin/article/delete',
+        'data-info': baseUrl + '/admin/data/info',
+        'data-article': baseUrl + '/admin/data/article',
+        'data-category': baseUrl + '/admin/data/category',
+        'data-visit': baseUrl + '/admin/data/visit',
+        'comment-search': baseUrl + '/admin/comment/search',
+        'comment-pass': baseUrl + '/admin/comment/pass',
+        'comment-reject': baseUrl + '/admin/comment/reject',
+        'comment-delete': baseUrl + '/admin/comment/delete'
+        
+    }
+    // 暴露接口
+    w.pathObj = pathObj;
+})(window)
