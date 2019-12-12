eafe6833308b79ef61f584ba6cb82678343673a9
caifu23 <1224758467@qq.com>
2019-12-12 20:49:54 +0800

    主页侧栏：热点、最新评论、周排行

--- js/index.js
+++ js/index.js
@@ -18,4 +18,41 @@ $(function() {
             $('.common_news').html(htmlNews);
         }
     });
+
+    //获取周评论排行
+    $.get({
+        url: 'http://localhost:8080/api/v1/index/rank',
+        success: function(res) {
+            // console.log(res)
+            if(res.code == 200 ) {
+                let html = template('weekRank', res);
+                $('.hotrank_list').html(html);
+
+            }
+        }
+
+    });
+
+    // 获取最新评论
+    $.get({
+        url: 'http://localhost:8080/api/v1/index/latest_comment',
+        success: function(res) {
+            // console.log(res)
+            if(res.code == 200) {
+                let html = template('recentNew', res);
+                $('.comment_list').html(html);
+            }
+        }
+    });
+
+    // 焦点关注
+    $.get({
+        url: 'http://localhost:8080/api/v1/index/attention ',
+        success: function(res) {
+            // console.log(res)
+            if(res.code == 200) {
+                $('.guanzhu_list').html(template('focusList', res))
+            }
+        }
+    });
 })
